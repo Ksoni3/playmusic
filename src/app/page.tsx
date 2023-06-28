bb12f7910dd3d2b27f82c224f4e77c6eb24e6ae4
Kamal Soni <94995608+Ksoni3@users.noreply.github.com>
2023-06-28 13:41:59 +1000

    home page finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 import Image from "next/image";
-import { Hero } from "./router";
+import Hero from "./components/hero/Hero";
+import Trending from "./components/hero/Trending";
+import LayoutSideBar from "./components/hero/LayoutSideBar";
+import Latest from "./components/hero/Latest";
+import Recommand from "./components/hero/Recommendation";
 
 export default function Home() {
   return (
     <>
+      <Hero />
+      <div>
+        <LayoutSideBar>
+          <Trending />
+          <Latest />
+          <Recommand />
+        </LayoutSideBar>
+      </div>
+
       <h1> Kamal SOni</h1>
     </>
   );
